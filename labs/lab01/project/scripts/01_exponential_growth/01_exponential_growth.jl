bee725a8b05e29c17bc64adac88ed8798f58e4fb
zmustafaev <1132231443@rudn.ru>
2026-02-18 13:32:45 +0300

    feat(main): add lab01

---
+++ labs/lab01/project/scripts/01_exponential_growth/01_exponential_growth.jl
@@ -0,0 +1,36 @@
+using DrWatson
+@quickactivate "project"
+using DifferentialEquations
+using Plots
+using DataFrames
+using JLD2
+script_name = splitext(basename(PROGRAM_FILE))[1]
+mkpath(plotsdir(script_name))
+mkpath(datadir(script_name))
+
+#𝑑𝑢/𝑑𝑡 = 𝛼𝑢, 𝑢(0) = 𝑢0
+function exponential_growth!(du, u, p, t)
+α = p
+du[1] = α * u[1]
+end
+
+u0 = [1.0] # начальная популяция
+α = 0.3 # скорость роста
+tspan = (0.0, 10.0) # временной интервал
+prob = ODEProblem(exponential_growth!, u0, tspan, α)
+sol = solve(prob, Tsit5(), saveat=0.1)
+
+plot(sol, label="u(t)", xlabel="Время t", ylabel="Популяция u",
+title="Экспоненциальный рост (α = $α)", lw=2, legend=:topleft)
+
+savefig(plotsdir(script_name, "exponential_growth_α=$α.png"))
+
+df = DataFrame(t=sol.t, u=first.(sol.u))
+println("Первые 5 строк результатов:")
+println(first(df, 5))
+
+u_final = last(sol.u)[1]
+doubling_time = log(2) / α
+println("\nАналитическое время удвоения: ", round(doubling_time; digits=2))
+
+@save datadir(script_name, "all_results.jld2") df
